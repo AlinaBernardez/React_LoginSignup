0517b03342aa96a221388b792f156db49dd253a2
AlinaB <bernardezalina@gmail.com>
2023-08-09 13:32:55 +0200

    final css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,8 @@ import styles from "../Home/HomeStyles.css"
 export function Home() {
     return (
         <div className="home_box">
-            <h2 className="home_title">Please login o create new account</h2>
+            <h1 className="home_hello">Hi there!</h1>
+            
             <div className="home">
                 <div className="home_btn">
                     <Link to="/login" className="home_link">Log in</Link>
@@ -15,6 +16,7 @@ export function Home() {
                     <Link to="/signup" className="home_link">Register</Link>
                 </div>
             </div>
+            <h2 className="home_title">Please login o create new account.</h2>
         </div>
     )
 }
--- src/components/Home/Welcome.jsx
+++ src/components/Home/Welcome.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
+import styles from "../Home/WelcomeStyles.css";
 
 export function Welcome(props) {
     const navigate = useNavigate();
@@ -9,11 +10,13 @@ export function Welcome(props) {
     }
     
     return (
-        <div className="welcome_box">
-            <h1 className="welcome_title">
-            {`Welcome ${props.name}!!`}
-            </h1>
-            <button onClick={salir} className="welcome_btn">Sign out</button>
+        <div className="welcome">
+            <div className="welcome_box">
+                <h1 className="welcome_title">
+                {`Hi ${props.name}, welcome back!`}
+                </h1>
+                <button onClick={salir} className="welcome_btn">Sign out</button>
+            </div>
         </div>
     )
 }
